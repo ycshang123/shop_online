1e42a16e2ef7907b7d288d6ec44e4b692f5ee915
ycshang <351234359@qq.com>
2023-11-11 15:47:16 +0800

    feat: 分类页-商品列表

--- src/main/java/com/soft2242/shop/controller/CategoryController.java
+++ src/main/java/com/soft2242/shop/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.soft2242.shop.controller;
 import com.soft2242.shop.common.result.Result;
 import com.soft2242.shop.entity.Category;
 import com.soft2242.shop.service.CategoryService;
+import com.soft2242.shop.vo.CategoryVO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
@@ -14,7 +15,7 @@ import java.util.List;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author ycshang
@@ -32,6 +33,14 @@ public class CategoryController {
     @Operation(summary = "首页-商品分类")
     public Result<List<Category>> getIndexCategoryList() {
         List<Category> list = categoryService.getIndexCategoryList();
+        return Result.ok(list);
+    }
+
+
+    @GetMapping("top")
+    @Operation(summary = "tab分类页-商品分类")
+    public Result<List<CategoryVO>> getList() {
+        List<CategoryVO> list = categoryService.getCategoryList();
         return Result.ok(list);
     }
 }
--- src/main/java/com/soft2242/shop/service/CategoryService.java
+++ src/main/java/com/soft2242/shop/service/CategoryService.java
@@ -2,6 +2,7 @@ package com.soft2242.shop.service;
 
 import com.soft2242.shop.entity.Category;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.soft2242.shop.vo.CategoryVO;
 
 import java.util.List;
 
@@ -21,5 +22,13 @@ public interface CategoryService extends IService<Category> {
      * @return
      */
     List<Category> getIndexCategoryList();
+
+
+    /**
+     * 分类tab页 - 分类列表以及各分类下的商品
+     *
+     * @return
+     */
+    List<CategoryVO> getCategoryList();
 
 }
