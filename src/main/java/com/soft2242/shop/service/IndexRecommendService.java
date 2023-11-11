1ce6b29e817e3215dc29d49f392c59eb723592e6
ycshang <351234359@qq.com>
2023-11-11 14:53:09 +0800

    feat: 首页推荐

--- src/main/java/com/soft2242/shop/service/IndexRecommendService.java
+++ src/main/java/com/soft2242/shop/service/IndexRecommendService.java
@@ -2,15 +2,25 @@ package com.soft2242.shop.service;
 
 import com.soft2242.shop.entity.IndexRecommend;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.soft2242.shop.vo.IndexRecommendVO;
+
+import java.util.List;
 
 /**
  * <p>
- *  服务类
+ * 服务类
  * </p>
  *
  * @author ycshang
  * @since 2023-11-07
  */
 public interface IndexRecommendService extends IService<IndexRecommend> {
+
+    /**
+     * 首页-热门推荐
+     *
+     * @return
+     */
+    List<IndexRecommendVO> getList();
 
 }
